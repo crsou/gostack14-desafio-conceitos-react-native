f31a3b71850b30b48d08383f33f236837ca2fc2d
Carlos <cvictor.rsouza@gmail.com>
2020-10-29 07:25:02 -0300

    desafio concluído

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://192.168.100.21:3333",
+  /* 
+  IOS c/ emulador: localhost
+  IOS c/ físico: IP da máquina
+  Android c/ emulador: (adb reverse tcp:3333 tcp: 3333) -> localhost
+  Android c/ emulador (2): 10.0.2.2 (Android Studio)
+  Android c/ emulador (3): 10.0.3.2 (Genymotion)
+  Android c/ físico: IP da máquina
+  */
 });
 
 export default api;
